2e38fc80f48042ca1f002d7030f418657904547a
s. galiamov <sgalyamov@gmail.com>
2018-11-08 22:43:32 +0100

    various changes

--- ILLightenComparer.Benchmark/Program.cs
+++ ILLightenComparer.Benchmark/Program.cs
@@ -1,12 +1,79 @@
 ﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 namespace ILLightenComparer.Benchmark
 {
-    class Program
+    [MedianColumn]
+    [RankColumn]
+    public class CompareSubVsIfs
     {
-        static void Main(string[] args)
+        private readonly int[] _one = new int[N];
+        private readonly int[] _other = new int[N];
+
+        private int _out;
+        private const int N = 10000;
+
+        [GlobalSetup]
+        public void Setup()
         {
-            Console.WriteLine("Hello World!");
+            if (b)
+            {
+                throw new Exception();
+            }
+
+            b = true;
+
+            var random = new Random();
+
+            for (var i = 0; i < N; i++)
+            {
+                _one[i] = random.Next(int.MinValue, int.MaxValue);
+                _other[i] = random.Next(int.MinValue, int.MaxValue);
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public void Sub() // faster 6 times
+        {
+            for (var i = 0; i < N; i++)
+            {
+                _out = _one[i] - _other[i];
+            }
+        }
+
+        [Benchmark]
+        public void Ifs()
+        {
+            for (var i = 0; i < N; i++)
+            {
+                var one = _one[i];
+                var other = _other[i];
+
+                if (one == other)
+                {
+                    _out = 0;
+                }
+
+                if (one > other)
+                {
+                    _out = 1;
+                }
+
+                _out = -1;
+            }
+        }
+
+        private static bool b;
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<CompareSubVsIfs>();
+
+            Console.WriteLine(summary);
         }
     }
 }
--- ILLightenComparer/Emit/ComparerEmitter.cs
+++ ILLightenComparer/Emit/ComparerEmitter.cs
@@ -17,29 +17,28 @@ namespace ILLightenComparer.Emit
 
         public IComparer Emit(Type objectType, CompareConfiguration configuration)
         {
-            var type = EmitType(objectType, configuration);
-
-            return Create<IComparer>(type);
-        }
-
-        public IComparer<T> Emit<T>(CompareConfiguration configuration)
-        {
-            var type = EmitType(typeof(T), configuration);
-
-            return Create<IComparer<T>>(type);
-        }
-
-        private T Create<T>(TypeBuilder type) => _emitter.EmitFactoryMethod<T>(type)();
-
-        private TypeBuilder EmitType(Type objectType, CompareConfiguration configuration)
-        {
             var type = _emitter.DefineType($"{objectType.FullName}.Comparer");
 
             var method = _emitter.DefineInterfaceMethod(type, Method.Compare);
 
             _methodEmitter.Emit(objectType, configuration, method);
 
-            return type;
+            return Create<IComparer>(type);
         }
+
+        public IComparer<T> Emit<T>(CompareConfiguration configuration)
+        {
+            var objectType = typeof(T);
+
+            var type = _emitter.DefineType($"{objectType.FullName}.Comparer");
+
+            var method = _emitter.DefineInterfaceMethod(type, Method.GenericCompare);
+
+            _methodEmitter.Emit(objectType, configuration, method);
+
+            return Create<IComparer<T>>(type);
+        }
+
+        private T Create<T>(TypeBuilder type) => _emitter.EmitFactoryMethod<T>(type)();
     }
 }
