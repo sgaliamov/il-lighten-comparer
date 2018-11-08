431bb7895314f3e4917c9da25b2e946284cd821b
s. galiamov <sgalyamov@gmail.com>
2018-11-08 17:05:50 +0100

    basim members provider

---
+++ ILightenComparer/Reflection/MembersProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+namespace ILightenComparer.Reflection
+{
+    internal sealed class MembersProvider
+    {
+        public MemberInfo[] GetMembers(Type type) =>
+            type.GetMembers(
+                BindingFlags.GetProperty
+                //| BindingFlags.GetField
+                //| BindingFlags.FlattenHierarchy
+                | BindingFlags.Public);
+    }
+}
