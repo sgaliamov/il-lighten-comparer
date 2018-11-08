e6337e3d1ff319ddba0d8bc9ababa6d26b375a55
s. galiamov <sgalyamov@gmail.com>
2018-11-08 21:29:07 +0100

    added ILLightenComparer.Benchmark

---
+++ ILLightenComparer.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ILLightenComparer.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
