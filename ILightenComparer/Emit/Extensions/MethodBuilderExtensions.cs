90e0c87ff569c45b50b707934dcd9ea3f74dd9b1
s. galiamov <sgalyamov@gmail.com>
2018-11-08 17:11:22 +0100

    emit factory and create delegate helpers

---
+++ ILightenComparer/Emit/Extensions/MethodBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace ILightenComparer.Emit.Extensions
+{
+    internal static class MethodBuilderExtensions
+    {
+        public static TDelegate CreateDelegate<TDelegate>(this MethodBuilder builder)
+            where TDelegate : Delegate
+            => (TDelegate)builder.CreateDelegate(typeof(TDelegate));
+    }
+}
