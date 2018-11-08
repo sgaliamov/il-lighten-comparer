d4608ce1caae4a02a5806869f5a3dd3f64ef3e7b
s. galiamov <sgalyamov@gmail.com>
2018-11-08 17:50:06 +0100

    reflection meta data

--- ILightenComparer/Reflection/Interface.cs
+++ ILightenComparer/Reflection/Interface.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 
 namespace ILightenComparer.Reflection
 {
-    public static class Interface
+    internal static class Interface
     {
         public static readonly Type Comparer = typeof(IComparer);
+        public static readonly Type EqualityComparer = typeof(IEqualityComparer);
         public static readonly Type GenericComparer = typeof(IComparer<>);
+        public static readonly Type GenericEqualityComparer = typeof(IEqualityComparer<>);
     }
 }
---
+++ ILightenComparer/Reflection/Method.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Reflection;
+
+namespace ILightenComparer.Reflection
+{
+    internal static class Method
+    {
+        public static MethodInfo Compare = Interface
+                                           .Comparer
+                                           .GetMethod(nameof(IComparer.Compare));
+
+        public static MethodInfo GenericCompare = Interface
+                                                  .GenericComparer
+                                                  .GetMethod(nameof(IComparer.Compare));
+
+        public static MethodInfo EqualsMethod = Interface
+                                                .EqualityComparer
+                                                .GetMethod(nameof(IEqualityComparer.Equals));
+
+        public static MethodInfo GenericEquals = Interface
+                                                 .GenericEqualityComparer
+                                                 .GetMethod(nameof(IEqualityComparer.Equals));
+
+        public static MethodInfo GetHashCodeMethod = Interface
+                                                     .EqualityComparer
+                                                     .GetMethod(nameof(IEqualityComparer.GetHashCode));
+
+        public static MethodInfo GenericGetHashCode = Interface
+                                                      .GenericEqualityComparer
+                                                      .GetMethod(nameof(IEqualityComparer.GetHashCode));
+    }
+}
