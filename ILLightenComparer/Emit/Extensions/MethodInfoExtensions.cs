330b72f58c742333c23194fb8966f1cc640bcbc9
s. galiamov <sgalyamov@gmail.com>
2018-11-16 09:31:36 +0100

    added TypeBuilderExtensions

--- ILLightenComparer/Emit/Extensions/MethodInfoExtensions.cs
+++ ILLightenComparer/Emit/Extensions/MethodInfoExtensions.cs
@@ -5,7 +5,10 @@ namespace ILLightenComparer.Emit.Extensions
 {
     internal static class MethodInfoExtensions
     {
-        public static TDelegate CreateDelegate<TDelegate>(this MethodInfo builder)
-            where TDelegate : Delegate => (TDelegate)builder.CreateDelegate(typeof(TDelegate));
+        public static TDelegate CreateDelegate<TDelegate>(this MethodInfo methodInfo)
+            where TDelegate : Delegate => (TDelegate)methodInfo.CreateDelegate(typeof(TDelegate));
+
+        public static TDelegate CreateDelegate<TDelegate, TTarget>(this MethodInfo methodInfo, TTarget target)
+            where TDelegate : Delegate => (TDelegate)methodInfo.CreateDelegate(typeof(TDelegate), target);
     }
 }
