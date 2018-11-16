1a1322048c245b14568a756bcd17f39354fbb7b0
s. galiamov <sgalyamov@gmail.com>
2018-11-16 23:47:05 +0100

    added test helpers

---
+++ ILLightenComparer.Tests/Comparers/ComparerTester.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
+using AutoFixture.Kernel;
+using FluentAssertions;
+using Force.DeepCloner;
+using ILLightenComparer.Tests.Utilities;
+
+namespace ILLightenComparer.Tests.Comparers
+{
+    internal static class ComparerTester
+    {
+        public static void TestAllFields<T>(IEqualityComparer<T> comparer)
+        {
+            TestAllFields(comparer, new string[0]);
+        }
+
+        public static void TestAllFields<T>(IEqualityComparer<T> comparer, string[] propertiesToIgnore)
+        {
+            var fixture = FixtureBuilder.GetInstance();
+            var context = new SpecimenContext(fixture);
+
+            var changes = typeof(T)
+                          .GetProperties()
+                          .Where(x => !propertiesToIgnore.Contains(x.Name))
+                          .Select(
+                              property =>
+                              {
+                                  var one = fixture.Create<T>();
+                                  var other = one.DeepClone();
+
+                                  var oldValue = property.GetValue(one);
+                                  var newValue = property.PropertyType == typeof(bool)
+                                      ? !(bool)oldValue
+                                      : context.Resolve(property.PropertyType);
+                                  property.SetValue(other, newValue);
+
+                                  return new
+                                  {
+                                      One = one,
+                                      Other = other,
+                                      PropertyName = property.Name,
+                                      OldValue = oldValue,
+                                      NewValue = newValue
+                                  };
+                              });
+
+            // act
+            var results = changes.Select(
+                                     x => new
+                                     {
+                                         ObjectsAreEquals = comparer.Equals(x.One, x.Other),
+                                         HashCodesAreEqual =
+                                             comparer.GetHashCode(x.One) == comparer.GetHashCode(x.Other),
+                                         ErrorMessage =
+                                             $"Property [{x.PropertyName}] is wrongly compared. Old value: {x.OldValue}, new value: {x.NewValue}."
+                                     })
+                                 .ToList();
+
+            // assert
+            results.ForEach(
+                x =>
+                {
+                    x.ObjectsAreEquals.Should().BeFalse(x.ErrorMessage);
+                    x.HashCodesAreEqual.Should().BeFalse(x.ErrorMessage);
+                });
+        }
+
+        public static void PositiveTest<T>(IEqualityComparer<T> comparer)
+        {
+            PositiveTest(comparer, new string[0]);
+        }
+
+        public static void PositiveTest<T>(IEqualityComparer<T> comparer, string[] propertiesToIgnore)
+        {
+            var fixture = FixtureBuilder.GetInstance();
+            var context = new SpecimenContext(fixture);
+
+            var one = fixture.Create<T>();
+            var other = one.DeepClone();
+
+            typeof(T)
+                .GetProperties()
+                .Where(x => propertiesToIgnore.Contains(x.Name))
+                .ToList()
+                .ForEach(
+                    property =>
+                    {
+                        var newValue = property.PropertyType == typeof(bool)
+                            ? !(bool)property.GetValue(other)
+                            : context.Resolve(property.PropertyType);
+
+                        property.SetValue(other, newValue);
+                    });
+
+            // act
+            var result = comparer.Equals(one, other);
+            var oneHashCode = comparer.GetHashCode(one);
+            var otherHashCode = comparer.GetHashCode(other);
+
+            // assert
+            result.Should().BeTrue();
+            oneHashCode.Should().Be(otherHashCode);
+        }
+    }
+}
---
+++ ILLightenComparer.Tests/Utilities/FixtureBuilder.cs
@@ -0,0 +1,9 @@
+﻿using AutoFixture;
+
+namespace ILLightenComparer.Tests.Utilities
+{
+    internal static class FixtureBuilder
+    {
+        public static Fixture GetInstance() => new Fixture();
+    }
+}
