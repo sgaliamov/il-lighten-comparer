f81e5a7628f2fc075111785f622b4de6f460c06c
s. galiamov <sgalyamov@gmail.com>
2018-11-19 16:44:38 +0100

    format string

--- ILLightenComparer.Tests/Samples/SampleStruct.cs
+++ ILLightenComparer.Tests/Samples/SampleStruct.cs
@@ -34,7 +34,7 @@ namespace ILLightenComparer.Tests.Samples
             }
         }
 
-        public override string ToString() => $"{nameof(Key)}: {Key}, {nameof(Value)}: {Value}";
+        public override string ToString() => $"{nameof(Key)}: {Key:+00;-00}, {nameof(Value)}: {Value:+00;-00}";
 
         public static IComparer Comparer { get; } = new SampleStructRelationalComparer();
     }
