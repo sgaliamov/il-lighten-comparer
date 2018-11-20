3bc27d1db94b7ff1fe491b2722ca7a614ba8dc11
s. galiamov <sgalyamov@gmail.com>
2018-11-20 21:15:36 +0100

    debug information for il emitter

--- ILLightenComparer/Emit/ILEmitter.cs
+++ ILLightenComparer/Emit/ILEmitter.cs
@@ -3,26 +3,49 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
 using System.Reflection.Emit;
+using ILLightenComparer.Emit.Extensions;
 
 namespace ILLightenComparer.Emit
 {
     internal sealed class ILEmitter
     {
-        private readonly Dictionary<Type, LocalBuilder> _cache = new Dictionary<Type, LocalBuilder>(0);
+#if DEBUG
+        private readonly List<Label> _labels = new List<Label>();
+#endif
+        private readonly Dictionary<Type, LocalBuilder> _locals = new Dictionary<Type, LocalBuilder>();
         private readonly ILGenerator _il;
 
         public ILEmitter(ILGenerator il) => _il = il;
 
-        public LocalBuilder GetLocal(Type localType) =>
-            _cache.TryGetValue(localType, out var local)
-                ? local
-                : _cache[localType] = _il.DeclareLocal(localType);
+        public LocalBuilder GetLocal(Type localType) => _locals.TryGetValue(localType, out var local)
+            ? local
+            : _locals[localType] = _il.DeclareLocal(localType);
 
-        public Label DefineLabel() => _il.DefineLabel();
+        public Label DefineLabel()
+        {
+            var label = _il.DefineLabel();
+#if DEBUG
+            _labels.Add(label);
+#endif
+            return label;
+        }
 
         public ILEmitter MarkLabel(Label label)
         {
+#if DEBUG
+            Debug.WriteLine("Label: " + _labels.IndexOf(label));
+#endif
             _il.MarkLabel(label);
+
+            return this;
+        }
+
+        public ILEmitter Emit(OpCode opCode, Label label)
+        {
+#if DEBUG
+            Debug.WriteLine($"{opCode} {_labels.IndexOf(label)}");
+#endif
+            _il.Emit(opCode, label);
 
             return this;
         }
@@ -33,15 +56,20 @@ namespace ILLightenComparer.Emit
                 ? OpCodes.Unbox_Any
                 : OpCodes.Castclass;
 
+            Debug.WriteLine($"{castOp} {objectType.Name}");
+
             _il.Emit(castOp, objectType);
 
             return this;
         }
 
-        public ILEmitter EmitCallCtor(ConstructorInfo constructor)
+        public ILEmitter EmitCtorCall(ConstructorInfo constructor) => EmitNew(constructor).Emit(OpCodes.Ret);
+
+        private ILEmitter EmitNew(ConstructorInfo constructorInfo)
         {
-            _il.Emit(OpCodes.Newobj, constructor);
-            _il.Emit(OpCodes.Ret);
+            _il.Emit(OpCodes.Newobj, constructorInfo);
+
+            Debug.WriteLine($"{OpCodes.Newobj} {constructorInfo.Name}");
 
             return this;
         }
@@ -49,6 +77,7 @@ namespace ILLightenComparer.Emit
         public ILEmitter Emit(OpCode opCode)
         {
             Debug.WriteLine(opCode.ToString());
+
             _il.Emit(opCode);
 
             return this;
@@ -56,6 +85,8 @@ namespace ILLightenComparer.Emit
 
         public ILEmitter Emit(OpCode opCode, MethodInfo methodInfo)
         {
+            Debug.WriteLine($"{opCode} {methodInfo.Name}");
+
             _il.Emit(opCode, methodInfo);
 
             return this;
@@ -63,21 +94,19 @@ namespace ILLightenComparer.Emit
 
         public ILEmitter Emit(OpCode opCode, int arg)
         {
+            Debug.WriteLine($"{opCode} {arg}");
+
             _il.Emit(opCode, arg);
-
-            return this;
-        }
-
-        public ILEmitter Emit(OpCode opCode, Label label)
-        {
-            _il.Emit(opCode, label);
 
             return this;
         }
 
         public ILEmitter EmitLoadAddress(LocalBuilder local)
         {
+            // todo: test
             var opCode = local.LocalIndex <= 255 ? OpCodes.Ldloca_S : OpCodes.Ldloca;
+
+            Debug.WriteLine($"{opCode} {local.LocalIndex}");
 
             _il.Emit(opCode, local);
 
@@ -86,7 +115,18 @@ namespace ILLightenComparer.Emit
 
         public ILEmitter EmitStore(LocalBuilder local)
         {
+            Debug.WriteLine($"{OpCodes.Stloc} {local.LocalIndex}");
+
             _il.Emit(OpCodes.Stloc, local); // todo: use short form
+
+            return this;
+        }
+
+        public ILEmitter Emit(OpCode opCode, FieldInfo field)
+        {
+            Debug.WriteLine($"{opCode} {field.DisplayName()}");
+
+            _il.Emit(opCode, field);
 
             return this;
         }
