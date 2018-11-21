e8277b9a345f68788a2d35171ce6c2e4a73986bd
s. galiamov <sgalyamov@gmail.com>
2018-11-21 16:35:21 +0100

    extracted MemberConverter

---
+++ ILLightenComparer/Emit/Reflection/MemberConverter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Reflection;
+using ILLightenComparer.Emit.Extensions;
+using ILLightenComparer.Emit.Members;
+
+namespace ILLightenComparer.Emit.Reflection
+{
+    internal sealed class MemberConverter
+    {
+        public Member Convert(MemberInfo memberInfo)
+        {
+            switch (memberInfo)
+            {
+                case FieldInfo field:
+                    return Convert(field);
+
+                case PropertyInfo property:
+                    return Convert(property);
+            }
+
+            throw new NotSupportedException(
+                "Only fields and properties are supported. "
+                + $"{memberInfo.MemberType}: {memberInfo.DisplayName()}");
+        }
+
+        private static Member Convert(PropertyInfo property)
+        {
+            if (property.PropertyType == typeof(string))
+            {
+                return new StringPropertyMember
+                {
+                    MemberType = property.PropertyType,
+                    Name = property.Name,
+                    OwnerType = property.DeclaringType,
+                    GetterMethod = property.GetMethod
+                };
+            }
+
+            return new PropertyMember
+            {
+                MemberType = GetUnderlyingType(property.PropertyType),
+                Name = property.Name,
+                GetterMethod = property.GetGetMethod(),
+                OwnerType = property.DeclaringType
+            };
+        }
+
+        private static Member Convert(FieldInfo field)
+        {
+            if (field.FieldType == typeof(string))
+            {
+                return new StringFiledMember
+                {
+                    MemberType = field.FieldType,
+                    Name = field.Name,
+                    OwnerType = field.DeclaringType,
+                    FieldInfo = field
+                };
+            }
+
+            return new FieldMember
+            {
+                MemberType = GetUnderlyingType(field.FieldType),
+                Name = field.Name,
+                OwnerType = field.DeclaringType,
+                FieldInfo = field
+            };
+        }
+
+        private static Type GetUnderlyingType(Type type) =>
+            type.IsEnum
+                ? Enum.GetUnderlyingType(type)
+                : type;
+    }
+}
--- ILLightenComparer/Emit/Reflection/MembersProvider.cs
+++ ILLightenComparer/Emit/Reflection/MembersProvider.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using ILLightenComparer.Emit.Extensions;
 using ILLightenComparer.Emit.Members;
 
 namespace ILLightenComparer.Emit.Reflection
 {
     internal sealed class MembersProvider
     {
+        private readonly MemberConverter _converter = new MemberConverter();
+
         public Member[] GetMembers(Type type, CompareConfiguration configuration) =>
             type.GetMembers(
                     BindingFlags.Instance
@@ -18,63 +19,8 @@ namespace ILLightenComparer.Emit.Reflection
                 .Where(memberInfo => IncludeFields(memberInfo, configuration.IncludeFields))
                 .OrderBy(x => x.MemberType) // todo: use functor from settings
                 .ThenBy(x => x.Name)
-                .Select(Convert)
+                .Select(_converter.Convert)
                 .ToArray();
-
-        private static Member Convert(MemberInfo memberInfo)
-        {
-            switch (memberInfo)
-            {
-                case FieldInfo field:
-                    if (field.FieldType == typeof(string))
-                    {
-                        return new StringFiledMember
-                        {
-                            MemberType = field.FieldType,
-                            Name = field.Name,
-                            OwnerType = field.DeclaringType,
-                            FieldInfo = field
-                        };
-                    }
-
-                    return new FieldMember
-                    {
-                        MemberType = GetUnderlyingType(field.FieldType),
-                        Name = field.Name,
-                        OwnerType = field.DeclaringType,
-                        FieldInfo = field
-                    };
-
-                case PropertyInfo property:
-                    if (property.PropertyType == typeof(string))
-                    {
-                        return new StringPropertyMember
-                        {
-                            MemberType = property.PropertyType,
-                            Name = property.Name,
-                            OwnerType = property.DeclaringType,
-                            GetterMethod = property.GetMethod
-                        };
-                    }
-
-                    return new PropertyMember
-                    {
-                        MemberType = GetUnderlyingType(property.PropertyType),
-                        Name = property.Name,
-                        GetterMethod = property.GetGetMethod(),
-                        OwnerType = property.DeclaringType
-                    };
-            }
-
-            throw new NotSupportedException(
-                "Only fields and properties are supported. "
-                + $"{memberInfo.MemberType}: {memberInfo.DisplayName()}");
-        }
-
-        private static Type GetUnderlyingType(Type type) =>
-            type.IsEnum
-                ? Enum.GetUnderlyingType(type)
-                : type;
 
         private static bool IgnoredMembers(MemberInfo memberInfo, ICollection<string> ignoredMembers) =>
             !ignoredMembers.Contains(memberInfo.Name);
