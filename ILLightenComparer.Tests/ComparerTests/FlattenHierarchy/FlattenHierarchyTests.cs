8acb4d1bfa548e5514ccd97c4c38ff63ffaea2fd
s. galiamov <sgalyamov@gmail.com>
2018-11-27 15:04:44 +0100

    created FlattenHierarchyTests

---
+++ ILLightenComparer.Tests/ComparerTests/FlattenHierarchy/FlattenHierarchyTests.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ILLightenComparer.Tests.ComparerTests.FlattenHierarchy
+{
+    public class FlattenHierarchyTests : BaseComparerTests<ChildObject>
+    {
+        protected override IComparer<ChildObject> ReferenceComparer { get; } =
+            ChildObject.ChildObjectComparer;
+    }
+}
---
+++ ILLightenComparer.Tests/ComparerTests/FlattenHierarchy/ParentObject.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using ILLightenComparer.Tests.Samples;
+
+namespace ILLightenComparer.Tests.ComparerTests.FlattenHierarchy
+{
+    public class ParentObject
+    {
+        public EnumSmall? EnumField;
+
+        public static IComparer<ParentObject> ParentObjectComparer { get; } = new ParentObjectRelationalComparer();
+        
+        public EnumBig? EnumProperty { get; set; }
+
+        private sealed class ParentObjectRelationalComparer : IComparer<ParentObject>
+        {
+            public int Compare(ParentObject x, ParentObject y)
+            {
+                if (ReferenceEquals(x, y))
+                {
+                    return 0;
+                }
+
+                if (ReferenceEquals(null, y))
+                {
+                    return 1;
+                }
+
+                if (ReferenceEquals(null, x))
+                {
+                    return -1;
+                }
+
+                var enumFieldComparison = Nullable.Compare(x.EnumField, y.EnumField);
+                if (enumFieldComparison != 0)
+                {
+                    return enumFieldComparison;
+                }
+
+                return Nullable.Compare(x.EnumProperty, y.EnumProperty);
+            }
+        }
+    }
+}
