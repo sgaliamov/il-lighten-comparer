52b549ba8e8ee8d31721fbc038089f43fc16b26e
s. galiamov <sgalyamov@gmail.com>
2018-12-07 22:12:03 +0100

    added ComparableStructTests

---
+++ ILLightenComparer.Tests/ComparerTests/ComparableTests/ComparableStructTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using AutoFixture;
+using FluentAssertions;
+using Force.DeepCloner;
+using ILLightenComparer.Tests.ComparerTests.ComparableTests.Samples;
+using Xunit;
+
+namespace ILLightenComparer.Tests.ComparerTests.ComparableTests
+{
+    public class ComparableStructTests : BaseComparerTests<ContainerStruct>
+    {
+        [Fact]
+        public void Replaced_Comparable_Object_Is_Compared()
+        {
+            var other = Fixture.Create<ContainerStruct>();
+
+            var one = other.DeepClone();
+            one.ComparableField = Fixture.Create<ChildComparableObject>();
+
+            var expected = ContainerStruct.Comparer.Compare(one, other);
+            var actual = TypedComparer.Compare(one, other);
+
+            actual.Should().Be(expected);
+        }
+
+        protected override IComparer<ContainerStruct> ReferenceComparer => ContainerStruct.Comparer;
+    }
+}
---
+++ ILLightenComparer.Tests/ComparerTests/ComparableTests/Samples/ContainerStruct.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ILLightenComparer.Tests.ComparerTests.ComparableTests.Samples
+{
+    public struct ContainerStruct
+    {
+        public ComparableObject ComparableField;
+        public ComparableStruct ComparableStructField;
+        public ComparableStruct? ComparableStructNullableField;
+
+        public static IComparer<ContainerStruct> Comparer { get; } = new RelationalComparer();
+
+        public ComparableObject ComparableProperty { get; set; }
+        public ComparableStruct? ComparableStructNullableProperty { get; set; }
+        public ComparableStruct ComparableStructProperty { get; set; }
+
+        private sealed class RelationalComparer : IComparer<ContainerStruct>
+        {
+            public int Compare(ContainerStruct x, ContainerStruct y)
+            {
+                var compare = Comparer<ComparableObject>.Default.Compare(
+                    x.ComparableField,
+                    y.ComparableField);
+                if (compare != 0) { return compare; }
+
+                compare = x.ComparableStructField.CompareTo(y.ComparableStructField);
+                if (compare != 0) { return compare; }
+
+                compare = Nullable.Compare(
+                    x.ComparableStructNullableField,
+                    y.ComparableStructNullableField);
+                if (compare != 0) { return compare; }
+
+                compare = Comparer<ComparableObject>.Default.Compare(
+                    x.ComparableProperty,
+                    y.ComparableProperty);
+                if (compare != 0) { return compare; }
+
+                compare = Nullable.Compare(
+                    x.ComparableStructNullableProperty,
+                    y.ComparableStructNullableProperty);
+                if (compare != 0) { return compare; }
+
+                compare = Comparer<ComparableStruct>.Default.Compare(
+                    x.ComparableStructProperty,
+                    y.ComparableStructProperty);
+                if (compare != 0) { return compare; }
+
+                return 0;
+            }
+        }
+    }
+}
