65f7de47afbfca622449a38f6ac11e49b0ed48be
s. galiamov <sgalyamov@gmail.com>
2018-12-17 16:32:38 +0100

    array item variable and comparison types

---
+++ ILLightenComparer/Emit/Emitters/Comparisons/ArrayItemComparison.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection.Emit;
+using ILLightenComparer.Emit.Emitters.Variables;
+using ILLightenComparer.Emit.Emitters.Visitors;
+
+namespace ILLightenComparer.Emit.Emitters.Comparisons
+{
+    internal sealed class ArrayItemComparison : IStackVisitorAcceptor, ICompareVisitorAcceptor
+    {
+        public ArrayItemComparison(ArrayItemVariable itemVariable, ICompareVisitorAcceptor itemAcceptor)
+        {
+            Variable = itemVariable;
+            ItemAcceptor = itemAcceptor;
+        }
+
+        public ICompareVisitorAcceptor ItemAcceptor { get; }
+
+        public ILEmitter Accept(CompareVisitor visitor, ILEmitter il)
+        {
+            return visitor.Visit(this, il);
+        }
+
+        public IVariable Variable { get; }
+
+        public ILEmitter LoadVariables(StackVisitor visitor, ILEmitter il, Label gotoNext)
+        {
+            return visitor.LoadVariables(this, il, gotoNext);
+        }
+    }
+}
---
+++ ILLightenComparer/Emit/Emitters/Variables/ArrayItemVariable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using ILLightenComparer.Emit.Emitters.Visitors;
+
+namespace ILLightenComparer.Emit.Emitters.Variables
+{
+    internal sealed class ArrayItemVariable : IVariable
+    {
+        public ArrayItemVariable(
+            Type ownerType,
+            Type variableType,
+            MethodInfo itemMethod,
+            LocalBuilder indexVariable)
+        {
+            OwnerType = ownerType;
+            VariableType = variableType;
+            GetItemMethod = itemMethod;
+            IndexVariable = indexVariable;
+        }
+
+        public MethodInfo GetItemMethod { get; }
+        public LocalBuilder IndexVariable { get; }
+
+        /// <summary>
+        ///     Where an array is defined.
+        /// </summary>
+        public Type OwnerType { get; }
+
+        /// <summary>
+        ///     Type of array element.
+        /// </summary>
+        public Type VariableType { get; }
+
+        public ILEmitter Load(VariableLoader visitor, ILEmitter il, ushort arg)
+        {
+            return visitor.Load(this, il, arg);
+        }
+
+        public ILEmitter LoadAddress(VariableLoader visitor, ILEmitter il, ushort arg)
+        {
+            return visitor.LoadAddress(this, il, arg);
+        }
+    }
+}
