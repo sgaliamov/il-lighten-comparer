156e0acb0a351b6a8cc007520ba4db80b5f004d0
s. galiamov <sgalyamov@gmail.com>
2018-12-24 18:01:39 +0100

    created SampleComparableTests

---
+++ ILLightenComparer.Tests/ComparerTests/SampleComparableTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using ILLightenComparer.Tests.Samples;
+using ILLightenComparer.Tests.Utilities;
+using Xunit;
+
+namespace ILLightenComparer.Tests.ComparerTests
+{
+    public sealed class SampleComparableTests
+    {
+        [Fact]
+        public void Compare_Sample_Comparable_Objects()
+        {
+            Test(typeof(SampleComparableBaseObject<>), nameof(SampleComparableBaseObject<object>.Comparer));
+            Test(typeof(SampleComparableChildObject<>), nameof(SampleComparableChildObject<object>.ChildComparer));
+        }
+
+        [Fact]
+        public void Compare_Sample_Comparable_Structs()
+        {
+            Test(typeof(SampleComparableStruct<>), nameof(SampleComparableStruct<object>.Comparer));
+        }
+
+        private static void Test(Type objectGenericType, string comparerName)
+        {
+            foreach (var item in SampleTypes.Types)
+            {
+                var objectType = objectGenericType.MakeGenericType(item.Key);
+
+                if (item.Value != null)
+                {
+                    objectType
+                        .GetField(comparerName, BindingFlags.Public | BindingFlags.Static)
+                        .SetValue(null, item.Value);
+                }
+
+
+                GenericTests.GenericTest(objectType, null, false, Constants.SmallCount);
+            }
+        }
+    }
+}
