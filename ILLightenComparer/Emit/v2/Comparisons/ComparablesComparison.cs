86ab4b7033ddeb4e33c918ace2be37cd3f225692
s. galiamov <sgalyamov@gmail.com>
2019-01-25 14:37:13 +0100

    moved CompareToMethod to ComparablesComparison

--- ILLightenComparer/Emit/v2/Comparisons/ComparablesComparison.cs
+++ ILLightenComparer/Emit/v2/Comparisons/ComparablesComparison.cs
@@ -1,5 +1,8 @@
-﻿using System.Reflection.Emit;
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
 using ILLightenComparer.Emit.Extensions;
+using ILLightenComparer.Emit.Reflection;
 using ILLightenComparer.Emit.Shared;
 using ILLightenComparer.Emit.v2.Variables;
 using ILLightenComparer.Emit.v2.Visitors;
@@ -11,8 +14,12 @@ namespace ILLightenComparer.Emit.v2.Comparisons
         private ComparablesComparison(IVariable variable)
         {
             Variable = variable;
+            CompareToMethod = variable.VariableType.GetUnderlyingCompareToMethod()
+                              ?? throw new ArgumentException(
+                                  $"{variable.VariableType.DisplayName()} does not have {MethodName.CompareTo} method.");
         }
 
+        public MethodInfo CompareToMethod { get; }
         public IVariable Variable { get; }
 
         public ILEmitter Accept(CompareVisitor visitor, ILEmitter il, Label gotoNext)
@@ -22,6 +29,7 @@ namespace ILLightenComparer.Emit.v2.Comparisons
 
         public static ComparablesComparison Create(IVariable variable)
         {
+            // todo: if object implements IComparable, then it should be used anyway?
             if (variable.VariableType.IsSealedComparable())
             {
                 return new ComparablesComparison(variable);
