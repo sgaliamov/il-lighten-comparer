d946057d82a3d80c1a069bbc6a9acb0034de86c5
s. galiamov <sgalyamov@gmail.com>
2019-03-18 21:32:25 +0100

    better tests

--- ILLightenComparer.Tests/ComparerTests/CustomComparerTests.cs
+++ ILLightenComparer.Tests/ComparerTests/CustomComparerTests.cs
@@ -13,72 +13,92 @@ namespace ILLightenComparer.Tests.ComparerTests
         [Fact]
         public void After_clean_custom_comparer_for_value_type_dynamic_comparer_should_be_created()
         {
-            var x = _fixture.Create<SampleObject<SampleStruct<string>>>();
-            var y = _fixture.Create<SampleObject<SampleStruct<string>>>();
+            Helper.Parallel(() =>
+                {
+                    var x = _fixture.Create<SampleObject<SampleStruct<string>>>();
+                    var y = _fixture.Create<SampleObject<SampleStruct<string>>>();
 
-            var referenceComparer = new SampleObjectComparer<SampleStruct<string>>(new SampleStructComparer<string>());
-            var expected = referenceComparer.Compare(x, y);
+                    var referenceComparer = new SampleObjectComparer<SampleStruct<string>>(new SampleStructComparer<string>());
+                    var expected = referenceComparer.Compare(x, y);
 
-            var builder = new ComparerBuilder();
-            var comparer = builder
-                           .SetComparer<SampleStructCustomComparer>()
-                           .GetComparer<SampleObject<SampleStruct<string>>>();
-            builder.SetComparer<SampleStruct<string>>(null);
+                    var builder = new ComparerBuilder();
+                    var comparer = builder
+                                   .SetComparer<SampleStructCustomComparer>()
+                                   .GetComparer<SampleObject<SampleStruct<string>>>();
+                    builder.SetComparer<SampleStruct<string>>(null);
 
-            comparer.Compare(x, y).Should().Be(expected);
+                    comparer.Compare(x, y).Should().Be(expected);
+                },
+                Constants.SmallCount);
         }
 
         [Fact]
         public void Custom_comparer_defined_as_a_type_should_be_used()
         {
-            var x = _fixture.Create<SampleObject<SampleStruct<string>>>();
-            var y = _fixture.Create<SampleObject<SampleStruct<string>>>();
+            Helper.Parallel(() =>
+                {
+                    var x = _fixture.Create<SampleObject<SampleStruct<string>>>();
+                    var y = _fixture.Create<SampleObject<SampleStruct<string>>>();
 
-            var comparer = new ComparerBuilder()
-                           .SetComparer<SampleStructCustomComparer>()
-                           .GetComparer<SampleObject<SampleStruct<string>>>();
+                    var comparer = new ComparerBuilder()
+                                   .SetComparer<SampleStructCustomComparer>()
+                                   .GetComparer<SampleObject<SampleStruct<string>>>();
 
-            comparer.Compare(x, y).Should().Be(0);
+                    comparer.Compare(x, y).Should().Be(0);
+                },
+                Constants.SmallCount);
         }
 
         [Fact]
         public void Custom_comparer_should_be_used_for_collection_when_defined()
         {
-            var x = _fixture.Create<SampleObject<int[]>>();
-            var y = _fixture.Create<SampleObject<int[]>>();
+            Helper.Parallel(() =>
+                {
+                    var x = _fixture.Create<SampleObject<int[]>>();
+                    var y = _fixture.Create<SampleObject<int[]>>();
 
-            var comparer = new ComparerBuilder()
-                           .SetComparer(new CustomisableComparer<int>((a, b) => 0))
-                           .GetComparer<SampleObject<int[]>>();
+                    var comparer = new ComparerBuilder()
+                                   .SetComparer(new CustomisableComparer<int>((a, b) => 0))
+                                   .GetComparer<SampleObject<int[]>>();
 
-            comparer.Compare(x, y).Should().Be(0);
+                    comparer.Compare(x, y).Should().Be(0);
+                },
+                Constants.SmallCount);
         }
 
         [Fact]
         public void Custom_comparer_should_be_used_for_itself()
         {
-            var x = _fixture.Create<SampleStruct<string>>();
-            var y = _fixture.Create<SampleStruct<string>>();
+            Helper.Parallel(() =>
+                {
+                    var x = _fixture.Create<SampleStruct<string>>();
+                    var y = _fixture.Create<SampleStruct<string>>();
 
-            var comparer = new ComparerBuilder()
-                           .SetComparer<SampleStructCustomComparer>()
-                           .GetComparer<SampleStruct<string>>();
+                    var comparer = new ComparerBuilder()
+                                   .SetComparer<SampleStructCustomComparer>()
+                                   .GetComparer<SampleStruct<string>>();
 
-            comparer.Compare(x, y).Should().Be(0);
+                    comparer.Compare(x, y).Should().Be(0);
+                },
+                Constants.SmallCount);
         }
 
         [Fact]
         public void Custom_instance_comparer_for_primitive_member_should_be_used()
         {
-            var x = _fixture.Create<Tuple<int, string>>();
-            var y = _fixture.Create<Tuple<int, string>>();
-            var expected = x.Item1.CompareTo(y.Item1);
+            Helper.Parallel(() =>
+                {
+                    var x = _fixture.Create<Tuple<int, string>>();
+                    var y = _fixture.Create<Tuple<int, string>>();
+                    var expected = x.Item1.CompareTo(y.Item1);
 
-            var comparer = new ComparerBuilder()
-                           .SetComparer(new CustomisableComparer<string>((a, b) => 0))
-                           .GetComparer<Tuple<int, string>>();
+                    var comparer = new ComparerBuilder()
+                                   .SetComparer(new CustomisableComparer<string>((a, b) => 0))
+                                   .GetComparer<Tuple<int, string>>();
 
-            comparer.Compare(x, y).Should().Be(expected);
+                    comparer.Compare(x, y).Should().Be(expected);
+                },
+                Constants.SmallCount);
         }
 
         private readonly Fixture _fixture = FixtureBuilder.GetInstance();
