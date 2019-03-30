995b1a824586ce1b8e280f735491f64bef52c133
s. galiamov <sgalyamov@gmail.com>
2019-03-30 20:31:04 +0100

    $(GitVersion_NuGetVersion)

--- src/ILLightenComparer/Shared/AssemblyInfo.cs
+++ src/ILLightenComparer/Shared/AssemblyInfo.cs
@@ -12,10 +12,10 @@ using System.Runtime.CompilerServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
+// //
+// [assembly: AssemblyVersion("1.0.0.0")]
+// [assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyInformationalVersion("1.0.0.0")]
