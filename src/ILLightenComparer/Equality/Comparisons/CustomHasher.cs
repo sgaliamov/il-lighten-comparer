41bb33052de26f47c23e3ed0dac58ee026737b76
s. galiamov <sgalyamov@gmail.com>
2020-03-13 22:14:18 +0100

    started equality tests

---
+++ src/ILLightenComparer/Equality/Comparisons/CustomHasher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection.Emit;
+using ILLightenComparer.Shared;
+using Illuminator;
+
+namespace ILLightenComparer.Equality.Comparisons
+{
+    internal sealed class CustomHasher : IStepEmitter
+    {
+        public bool PutsResultInStack { get; }
+
+        public ILEmitter Emit(ILEmitter il, Label gotoNext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILEmitter Emit(ILEmitter il) => Emit(il, default).Return();
+    }
+}
