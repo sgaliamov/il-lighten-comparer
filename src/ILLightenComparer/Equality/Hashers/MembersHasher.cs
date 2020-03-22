d2ba2283601d592378df727a12678ba98a8a250f
s. galiamov <sgalyamov@gmail.com>
2020-03-22 17:16:10 +0100

    implemented hasing

--- src/ILLightenComparer/Equality/Hashers/MembersHasher.cs
+++ src/ILLightenComparer/Equality/Hashers/MembersHasher.cs
@@ -1,14 +1,17 @@
 ﻿using System.Linq;
+using ILLightenComparer.Config;
 using ILLightenComparer.Extensions;
 using ILLightenComparer.Reflection;
 using ILLightenComparer.Shared;
 using ILLightenComparer.Variables;
 using Illuminator;
+using static Illuminator.Functional;
 
 namespace ILLightenComparer.Equality.Hashers
 {
     internal sealed class MembersHasher : IHasherEmitter
     {
+        private readonly IConfigurationProvider _configuration;
         private readonly MembersProvider _membersProvider;
         private readonly HasherResolver _resolver;
         private readonly IVariable _variable;
@@ -16,20 +19,23 @@ namespace ILLightenComparer.Equality.Hashers
         private MembersHasher(
             HasherResolver resolver,
             MembersProvider membersProvider,
+            IConfigurationProvider configuration,
             IVariable variable)
         {
             _variable = variable;
             _resolver = resolver;
             _membersProvider = membersProvider;
+            _configuration = configuration;
         }
 
         public static MembersHasher Create(
             HasherResolver resolver,
             MembersProvider membersProvider,
+            IConfigurationProvider configuration,
             IVariable variable)
         {
             if (variable.VariableType.IsHierarchical() && variable is ArgumentVariable) {
-                return new MembersHasher(resolver, membersProvider, variable);
+                return new MembersHasher(resolver, membersProvider, configuration, variable);
             }
 
             return null;
@@ -43,9 +49,18 @@ namespace ILLightenComparer.Equality.Hashers
                 .GetMembers(variableType)
                 .Select(_resolver.GetHasher);
 
+            var config = _configuration.Get(variableType);
+
+            il.LoadLong(config.HashSeed)
+              .Store(typeof(long), out var seed);
+
             foreach (var item in comparisons) {
                 using (il.LocalsScope()) {
-                    item.Emit(il);
+                    var add = Add(
+                        ShiftLeft(LoadLocal(seed), LoadInteger(5)),
+                        LoadLong(config.HashSeed));
+
+                    Xor(add, Execute(item.Emit) + Cast(typeof(long)))(il);
                 }
             }
 
