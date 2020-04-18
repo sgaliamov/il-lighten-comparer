ded020e03deec010d7c301c7c48bfd6a58893341
s. galiamov <salavat.galiamov@neocles.io>
2020-04-18 23:33:21 +0200

    better enum generation

--- src/ILLightenComparer.Tests/Utilities/ThreadSafeRandom.cs
+++ src/ILLightenComparer.Tests/Utilities/ThreadSafeRandom.cs
@@ -5,20 +5,14 @@ namespace ILLightenComparer.Tests.Utilities
     internal sealed class ThreadSafeRandom
     {
         private static readonly Random Global = new Random();
+
         [ThreadStatic] private static Random _local;
 
         public static double NextDouble() => GetInstance().NextDouble();
 
         public static int Next(int minValue, int maxValue) => GetInstance().Next(minValue, maxValue);
 
-        private static Random GetInstance()
-        {
-            if (_local != null) {
-                return _local;
-            }
-
-            return _local = new Random(GetSeed());
-        }
+        private static Random GetInstance() => _local ?? (_local = new Random(GetSeed()));
 
         private static int GetSeed()
         {
