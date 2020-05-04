f64077ed6455e4f024a0301c2616e28f43ff057a
s. galiamov <sgalyamov@gmail.com>
2020-05-04 18:53:59 +0200

    created SampleEquatableTests

---
+++ src/ILLightenComparer.Tests/EqualityTests/SampleEquatableTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using Xunit;
+
+namespace ILLightenComparer.Tests.EqualityTests
+{
+    public sealed class SampleEquatableTests
+    {
+        [Fact]
+        public void Custom_IEquatable_implementation_is_not_used_yet()
+        {
+            var x = new TrueEquatable { Property = 1 };
+            var y = new TrueEquatable { Property = 2 };
+
+            var comparer = new ComparerBuilder().GetEqualityComparer<TrueEquatable>();
+
+            comparer.Equals(x, y).Should().BeFalse();
+        }
+
+        public sealed class TrueEquatable : IEquatable<TrueEquatable>
+        {
+            public int Property { get; set; }
+
+            public bool Equals([AllowNull] TrueEquatable other) => true;
+        }
+    }
+}
