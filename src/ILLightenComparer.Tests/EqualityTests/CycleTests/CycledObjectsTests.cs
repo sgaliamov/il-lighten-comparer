e0d0e4fb836c92a5b077bd61e959cd19d7c6c09d
s. galiamov <sgalyamov@gmail.com>
2020-05-10 22:51:30 +0200

    enabled CycledObjectsTests

--- src/ILLightenComparer.Tests/EqualityTests/CycleTests/CycledObjectsTests.cs
+++ src/ILLightenComparer.Tests/EqualityTests/CycleTests/CycledObjectsTests.cs
@@ -5,6 +5,7 @@ using FluentAssertions;
 using FluentAssertions.Execution;
 using Force.DeepCloner;
 using ILLightenComparer.Tests.EqualityTests.CycleTests.Samples;
+using ILLightenComparer.Tests.Samples;
 using ILLightenComparer.Tests.Utilities;
 using Xunit;
 
@@ -19,17 +20,6 @@ namespace ILLightenComparer.Tests.EqualityTests.CycleTests
         }
 
         [Fact]
-        public void Comparison_should_not_fail_because_of_generating_comparers_for_two_dependent_classes()
-        {
-            var one = _fixture.Create<OneSealed>();
-            var other = _fixture.Create<OneSealed>();
-            one.Two.Three.One = one;
-            other.Two.Three.One = one;
-
-            ComparerForOneSealed.Equals(one, other).Should().BeFalse();
-        }
-
-        [Fact]
         public void Hasing_should_fail_because_of_generating_comparers_for_two_dependent_classes()
         {
             var one = _fixture.Create<OneSealed>();
@@ -38,6 +28,7 @@ namespace ILLightenComparer.Tests.EqualityTests.CycleTests
             other.Two.Three.One = one;
 
             using (new AssertionScope()) {
+                ComparerForOneSealed.Equals(one, other).Should().BeFalse();
                 Assert.Throws<ArgumentException>(() => ComparerForOneSealed.GetHashCode(one));
                 Assert.Throws<ArgumentException>(() => ComparerForOneSealed.GetHashCode(other));
             }
@@ -101,207 +92,203 @@ namespace ILLightenComparer.Tests.EqualityTests.CycleTests
             });
         }
 
-        //[Fact]
-        //public void Detects_cycle_on_second_member()
-        //{
-        //    var one = new SelfSealed { Value = 1 };
-        //    one.Second = new SelfSealed {
-        //        First = one,
-        //        Value = 2
-        //    };
-        //    /*
-        //          1
-        //         / \
-        //        N   2
-        //    cycle: / \
-        //          1   N
-        //         / \ 
-        //        N   2
-        //    */
+        [Fact]
+        public void Detects_cycle_on_second_member()
+        {
+            var one = new SelfSealed { Value = 1 };
+            one.Second = new SelfSealed {
+                First = one,
+                Value = 2
+            };
+            /*
+                  1
+                 / \
+                N   2
+            cycle: / \
+                  1   N
+                 / \ 
+                N   2
+            */
 
-        //    var other = new SelfSealed {
-        //        Second = new SelfSealed {
-        //            First = new SelfSealed { Value = 3 },
-        //            Value = 4
-        //        },
-        //        Value = 5
-        //    };
-        //    /*
-        //          3
-        //         / \
-        //        N   4
-        //           / \
-        //          5   N
-        //         / \
-        //        N   N difference here: 2 > N
-        //    */
+            var other = new SelfSealed {
+                Second = new SelfSealed {
+                    First = new SelfSealed { Value = 3 },
+                    Value = 4
+                },
+                Value = 5
+            };
+            /*
+                  3
+                 / \
+                N   4
+                   / \
+                  5   N
+                 / \
+                N   N difference here: 2 > N
+            */
 
-        //    var expected = SelfSealed.Comparer.Equals(one, other);
-        //    var actual = ComparerSelfSealed.Equals(one, other);
+            using (new AssertionScope()) {
+                Assert.Throws<ArgumentException>(() => ComparerSelfSealed.GetHashCode(one));
+                ComparerSelfSealed.GetHashCode(other).Should().Be(-1670556025);
+                ComparerSelfSealed.Equals(one, other).Should().BeFalse();
+            }
+        }
 
-        //    using (new AssertionScope()) {
-        //        expected.Should().Be(1);
-        //        actual.Should().Be(expected);
-        //    }
-        //}
+        [Fact]
+        public void No_cycle_when_identical_objects_in_collection()
+        {
+            var one = new SelfSealed { Value = 1 };
+            one.Second = new SelfSealed {
+                First = one,
+                Value = 1
+            };
+            var two = new SelfSealed { Value = 1 };
+            two.Second = new SelfSealed {
+                First = two,
+                Value = 1
+            };
+            var x = new[] {
+                one,
+                one,
+                one
+            };
 
-        //[Fact]
-        //public void No_cycle_when_identical_objects_in_collection()
-        //{
-        //    var one = new SelfSealed { Value = 1 };
-        //    one.Second = new SelfSealed {
-        //        First = one,
-        //        Value = 1
-        //    };
-        //    var two = new SelfSealed { Value = 1 };
-        //    two.Second = new SelfSealed {
-        //        First = two,
-        //        Value = 1
-        //    };
-        //    var x = new[] {
-        //        one,
-        //        one,
-        //        one
-        //    };
+            var other = new SelfSealed {
+                Second = new SelfSealed {
+                    First = new SelfSealed {
+                        Value = 4
+                    },
+                    Value = 4
+                },
+                Value = 3
+            };
+            var y = new[] {
+                two,
+                two,
+                other
+            };
 
-        //    var other = new SelfSealed {
-        //        Second = new SelfSealed {
-        //            First = new SelfSealed {
-        //                Value = 4
-        //            },
-        //            Value = 4
-        //        },
-        //        Value = 3
-        //    };
-        //    var y = new[] {
-        //        two,
-        //        two,
-        //        other
-        //    };
+            var comparer = new ComparerBuilder().For<SelfSealed>().GetEqualityComparer<SelfSealed[]>();
 
-        //    var expected = SelfSealed.Comparer.Equals(x, y);
-        //    var comparer = new ComparerBuilder().For<SelfSealed>(c => c.IgnoreMember(o => o.Id)).GetComparer<SelfSealed[]>();
-        //    var actual = comparer.Equals(x, y);
+            using (new AssertionScope()) {
+                Assert.Throws<ArgumentException>(() => comparer.Equals(x, y));
+                Assert.Throws<ArgumentException>(() => comparer.GetHashCode(x));
+                Assert.Throws<ArgumentException>(() => comparer.GetHashCode(y));
+            }
+        }
 
-        //    using (new AssertionScope()) {
-        //        expected.Should().Be(-1);
-        //        actual.Should().Be(-1);
-        //    }
-        //}
+        [Fact]
+        public void Detects_cycle_handles_equalty_by_value()
+        {
+            var one = new SelfSealed { Value = 1 };
+            one.Second = new SelfSealed {
+                First = one,
+                Value = 1
+            };
+            /*
+                  1-1
+                 / \
+                N   2-1
+            cycle: / \
+                  1-1 N
+                 / \ 
+                N   2-1
+            */
 
-        //[Fact]
-        //public void Detects_cycle_handles_equalty_by_value()
-        //{
-        //    var one = new SelfSealed { Value = 1 };
-        //    one.Second = new SelfSealed {
-        //        First = one,
-        //        Value = 1
-        //    };
-        //    /*
-        //          1-1
-        //         / \
-        //        N   2-1
-        //    cycle: / \
-        //          1-1 N
-        //         / \ 
-        //        N   2-1
-        //    */
+            var other = new SelfSealed {
+                Second = new SelfSealed {
+                    First = new SelfSealed {
+                        Second = new SelfSealed { Value = 0 },
+                        Value = 4
+                    },
+                    Value = 4
+                },
+                Value = 3
+            };
+            /*
+                  3-3
+                 / \
+                N   4-4
+                   / \
+                  5-4 N
+                 / \
+                N   6-6
+            */
 
-        //    var other = new SelfSealed {
-        //        Second = new SelfSealed {
-        //            First = new SelfSealed {
-        //                Second = new SelfSealed { Value = 0 },
-        //                Value = 4
-        //            },
-        //            Value = 4
-        //        },
-        //        Value = 3
-        //    };
-        //    /*
-        //          3-3
-        //         / \
-        //        N   4-4
-        //           / \
-        //          5-4 N
-        //         / \
-        //        N   6-6
-        //    */
+            using (new AssertionScope()) {
+                Assert.Throws<ArgumentException>(() => ComparerSelfSealed.GetHashCode(one));
+                ComparerSelfSealed.GetHashCode(other).Should().Be(-1064642813);
+                ComparerSelfSealed.Equals(one, other).Should().BeFalse();
+            }
+        }
 
-        //    var expected = SelfSealed.Comparer.Equals(one, other);
-        //    var actual = ComparerSelfSealed.Equals(one, other);
+        [Fact]
+        public void Object_with_bigger_cycle_is_bigger()
+        {
+            var one = new SelfSealed();
+            one.First = new SelfSealed {
+                First = new SelfSealed {
+                    First = one
+                }
+            };
+            var other = new SelfSealed { First = one };
 
-        //    using (new AssertionScope()) {
-        //        expected.Should().Be(-1);
-        //        actual.Should().Be(expected);
-        //    }
-        //}
+            using (new AssertionScope()) {
+                Assert.Throws<ArgumentException>(() => ComparerSelfSealed.GetHashCode(one));
+                Assert.Throws<ArgumentException>(() => ComparerSelfSealed.GetHashCode(other));
+                Assert.Throws<ArgumentException>(() => ComparerSelfSealed.Equals(one, other));
+            }
+        }
 
-        //[Fact]
-        //public void Object_with_bigger_cycle_is_bigger()
-        //{
-        //    var one = new SelfSealed();
-        //    one.First = new SelfSealed {
-        //        First = new SelfSealed {
-        //            First = one
-        //        }
-        //    };
-        //    var other = new SelfSealed { First = one };
+        [Fact]
+        public void Opened_class_comparer_uses_context_compare_method()
+        {
+            var one = _fixture.Create<SelfOpened>();
+            one.Self = one;
+            var other = _fixture.Create<SelfOpened>();
+            other.Self = other;
 
-        //    var expected = SelfSealed.Comparer.Equals(one, other);
-        //    var actual = ComparerSelfSealed.Equals(one, other);
+            using (new AssertionScope()) {
+                Assert.Throws<ArgumentException>(() => ComparerSelfOpened.GetHashCode(one));
+                Assert.Throws<ArgumentException>(() => ComparerSelfOpened.GetHashCode(other));
+                Assert.Throws<ArgumentException>(() => ComparerSelfOpened.Equals(one, other));
+            }
+        }
 
-        //    actual.Should().Be(expected);
-        //}
+        [Fact]
+        public void When_sealed_comparable_has_member_with_cycle()
+        {
+            var comparer = _builder.For<ComparableChildObject<OneSealed>>().GetEqualityComparer();
+            ComparableChildObject<OneSealed>.ChildComparer = _builder.GetComparer<OneSealed>();
 
-        //[Fact]
-        //public void Opened_class_comparer_uses_context_compare_method()
-        //{
-        //    var one = _fixture.Create<SelfOpened>();
-        //    one.Self = one;
-        //    var other = _fixture.Create<SelfOpened>();
-        //    other.Self = other;
+            var one = _fixture.Create<OneSealed>();
+            var other = _fixture.Create<OneSealed>();
+            one.Value = other.Value;
+            one.Two.Three.One = one;
+            other.Two.Three.One = _fixture.Create<OneSealed>();
+            other.Two.Three.One.Value = one.Value;
+            other.Two.Three.One.Two.Three.One = other;
+            var x = new ComparableChildObject<OneSealed> {
+                ChildField = null,
+                ChildProperty = other
+            };
+            var y = new ComparableChildObject<OneSealed> {
+                ChildField = null,
+                ChildProperty = one
+            };
 
-        //    var expected = one.Value.CompareTo(other.Value);
-        //    var actual = ComparerSelfOpened.Equals(one, other);
+            using (new AssertionScope()) {
+                Assert.Throws<ArgumentException>(() => comparer.GetHashCode(x));
+                Assert.Throws<ArgumentException>(() => comparer.GetHashCode(y));
+                Assert.Throws<ArgumentException>(() => comparer.Equals(x, y));
+            }
+        }
 
-        //    actual.Should().Be(expected);
-        //}
-
-        //[Fact]
-        //public void When_sealed_comparable_has_member_with_cycle()
-        //{
-        //    var comparer = _builder.For<ComparableChildObject<OneSealed>>().GetComparer();
-        //    ComparableChildObject<OneSealed>.ChildComparer = ComparerForOneSealed;
-
-        //    var one = _fixture.Create<OneSealed>();
-        //    var other = _fixture.Create<OneSealed>();
-        //    one.Value = other.Value;
-        //    one.Two.Three.One = one;
-        //    other.Two.Three.One = _fixture.Create<OneSealed>();
-        //    other.Two.Three.One.Value = one.Value;
-        //    other.Two.Three.One.Two.Three.One = other;
-        //    var x = new ComparableChildObject<OneSealed> {
-        //        ChildField = null,
-        //        ChildProperty = other
-        //    };
-        //    var y = new ComparableChildObject<OneSealed> {
-        //        ChildField = null,
-        //        ChildProperty = one
-        //    };
-
-        //    var expected = ComparerForOneSealed.Equals(other, one);
-        //    var actual = comparer.Equals(x, y);
-
-        //    using (new AssertionScope()) {
-        //        actual.Should().Be(expected);
-        //        actual.Should().BePositive();
-        //    }
-        //}
+        private readonly Fixture _fixture;
+        private readonly IComparerBuilder _builder = new ComparerBuilder();
 
         private IEqualityComparer<SelfSealed> ComparerSelfSealed => _builder.For<SelfSealed>().GetEqualityComparer();
-        private readonly Fixture _fixture;
         private IEqualityComparer<SelfOpened> ComparerSelfOpened => _builder.For<SelfOpened>().GetEqualityComparer();
         private IEqualityComparer<OneSealed> ComparerForOneSealed => _builder.For<OneSealed>().GetEqualityComparer();
-        private readonly IComparerBuilder _builder = new ComparerBuilder();
     }
 }
